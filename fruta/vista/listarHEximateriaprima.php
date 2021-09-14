11ef698317ef43da18598e3354471f5579e1d1f5
Gabriel Toledo muñoz <gabrielvalkunut94@gmail.com>
2021-09-14 17:33:20 -0300

    revisión formato de fechas

--- fruta/vista/listarHEximateriaprima.php
+++ fruta/vista/listarHEximateriaprima.php
@@ -343,8 +343,8 @@ if ($EMPRESAS  && $PLANTAS && $TEMPORADAS) {
                                                             <td><?php echo $r['DESPACHO']; ?></td>
                                                             <td><?php echo $NUMEROGUIADESPACHO; ?></td>
                                                             <td><?php echo $r['DIAS']; ?></td>
-                                                            <td><?php echo $r['INGRESOF']; ?></td>
-                                                            <td><?php echo $r['MODIFICACIONF']; ?></td>
+                                                            <td><?php echo $r['INGRESO']; ?></td>
+                                                            <td><?php echo $r['MODIFICACION']; ?></td>
                                                             <td><?php echo $NOMBREEMPRESA; ?></td>
                                                             <td><?php echo $NOMBREPLANTA; ?></td>
                                                             <td><?php echo $NOMBRETEMPORADA; ?></td>
