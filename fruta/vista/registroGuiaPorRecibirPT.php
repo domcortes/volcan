537ec2d245a056532b3fbf64d1b353041403cb8c
Gabriel Toledo muñoz <gabrielvalkunut94@gmail.com>
2021-09-17 10:03:31 -0300

    el proceso de aprobar y rechazar se esa en la misma vista

--- fruta/vista/registroGuiaPorRecibirPT.php
+++ fruta/vista/registroGuiaPorRecibirPT.php
@@ -56,7 +56,7 @@ $DISABLEDFOLIO = "";
 //INICIALIZAR ARREGLOS
 $ARRAYDESPACHOPT = "";
 $ARRAYDESPACHOPTTOTALES = "";
-$ARRAYVEREMPRESA = "";  
+$ARRAYVEREMPRESA = "";
 $ARRAYVERPRODUCTOR = "";
 $ARRAYVERTRANSPORTE = "";
 $ARRAYVERCONDUCTOR = "";
@@ -84,7 +84,71 @@ if ($EMPRESAS  && $PLANTAS && $TEMPORADAS) {
 include_once "../config/validarDatosUrl.php";
 include_once "../config/datosUrLP.php";
 
+//OPERACIONES
+if (isset($_REQUEST['APROBARURL'])) {
 
+    $DESPACHOPT->__SET('ID_DESPACHO', $_REQUEST['ID']);
+    //LLAMADA AL METODO DE EDITAR DEL CONTROLADOR
+    $DESPACHOPT_ADO->cerrado($DESPACHOPT);
+
+    $DESPACHOPT->__SET('ID_DESPACHO', $_REQUEST['ID']);
+    //LLAMADA AL METODO DE EDITAR DEL CONTROLADOR
+    $DESPACHOPT_ADO->Aprobado($DESPACHOPT);
+
+    $ARRAYEXISENCIADESPACHOMP = $EXIEXPORTACION_ADO->verExistenciaPorDespachoEnTransito($_REQUEST['ID']);
+    foreach ($ARRAYEXISENCIADESPACHOMP as $r) :
+        $EXIEXPORTACION->__SET('ID_EXIEXPORTACION', $r['ID_EXIEXPORTACION']);
+        //LLAMADA AL METODO DE EDITAR DEL CONTROLADOR
+        $EXIEXPORTACION_ADO->despachadoInterplanta($EXIEXPORTACION);
+    endforeach;
+
+    $ARRAYVERFOLIO = $FOLIO_ADO->verFolioPorEmpresaPlantaTemporadaTexportacion($EMPRESAS, $PLANTAS, $TEMPORADAS);
+    $FOLIO = $ARRAYVERFOLIO[0]['ID_FOLIO'];
+
+    foreach ($ARRAYEXISENCIADESPACHOMP as $r) :
+        $EXIEXPORTACION->__SET('FOLIO_EXIEXPORTACION',  $r['FOLIO_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('FOLIO_AUXILIAR_EXIEXPORTACION', $r['FOLIO_AUXILIAR_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('FOLIO_MANUAL', $r['FOLIO_MANUAL']);
+        $EXIEXPORTACION->__SET('FECHA_EMBALADO_EXIEXPORTACION', $r['FECHA_EMBALADO_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('CANTIDAD_ENVASE_EXIEXPORTACION', $r['CANTIDAD_ENVASE_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('KILOS_NETO_EXIEXPORTACION', $r['KILOS_NETO_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('KILOS_BRUTO_EXIEXPORTACION', $r['KILOS_BRUTO_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('PDESHIDRATACION_EXIEXPORTACION', $r['PDESHIDRATACION_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('KILOS_DESHIRATACION_EXIEXPORTACION', $r['KILOS_DESHIRATACION_EXIEXPORTACION']);
+        $EXIEXPORTACION->__SET('OBSERVACION_EXIESPORTACION', $r['OBSERVACION_EXIESPORTACION']);
+        $EXIEXPORTACION->__SET('ALIAS_DINAMICO_FOLIO_EXIESPORTACION', $r['ALIAS_DINAMICO_FOLIO_EXIESPORTACION']);
+        $EXIEXPORTACION->__SET('ALIAS_ESTATICO_FOLIO_EXIESPORTACION', $r['ALIAS_ESTATICO_FOLIO_EXIESPORTACION']);
+        $EXIEXPORTACION->__SET('STOCK', $r['STOCK']);
+        $EXIEXPORTACION->__SET('EMBOLSADO', $r['EMBOLSADO']);
+        $EXIEXPORTACION->__SET('GASIFICADO', $r['GASIFICADO']);
+        $EXIEXPORTACION->__SET('PREFRIO', $r['PREFRIO']);
+        $EXIEXPORTACION->__SET('TESTADOSAG', $r['TESTADOSAG']);
+        $EXIEXPORTACION->__SET('VGM', $r['VGM']);
+        $EXIEXPORTACION->__SET('INGRESO', $r['INGRESO']);
+        $EXIEXPORTACION->__SET('ID_TCALIBRE', $r['ID_TCALIBRE']);
+        $EXIEXPORTACION->__SET('ID_TEMBALAJE', $r['ID_TEMBALAJE']);
+        $EXIEXPORTACION->__SET('ID_TMANEJO', $r['ID_TMANEJO']);
+        $EXIEXPORTACION->__SET('ID_FOLIO', $FOLIO);
+        $EXIEXPORTACION->__SET('ID_ESTANDAR', $r['ID_ESTANDAR']);
+        $EXIEXPORTACION->__SET('ID_PRODUCTOR', $r['ID_PRODUCTOR']);
+        $EXIEXPORTACION->__SET('ID_VESPECIES', $r['ID_VESPECIES']);
+        $EXIEXPORTACION->__SET('ID_PLANTA2', $r['ID_PLANTA']);
+        $EXIEXPORTACION->__SET('ID_DESPACHO2', $_REQUEST['ID']);
+        $EXIEXPORTACION->__SET('ID_EMPRESA', $EMPRESAS);
+        $EXIEXPORTACION->__SET('ID_PLANTA', $PLANTAS);
+        $EXIEXPORTACION->__SET('ID_TEMPORADA', $TEMPORADAS);
+        //LLAMADA AL METODO DE REGISTRO DEL CONTROLADOR
+        $EXIEXPORTACION_ADO->agregarExiexportacionGuia($EXIEXPORTACION);
+    endforeach;
+    echo "<script type='text/javascript'> location.href ='" . $_REQUEST['URLO'] . ".php?op';</script>";
+}
+
+if (isset($_REQUEST['RECHAZARURL'])) {
+    $_SESSION["parametro"] = $_REQUEST['ID'];
+    $_SESSION["parametro1"] = "rechazar";
+    $_SESSION["urlO"] = $_REQUEST['URLO'];
+    echo "<script type='text/javascript'> location.href ='" . $_REQUEST['URLM'] . ".php?op';</script>";
+}
 
 
 
@@ -336,12 +400,12 @@ include_once "../config/datosUrLP.php";
                                                                             <input type="hidden" class="form-control" placeholder="URL" id="URLM" name="URLM" value="registroGuiaPorRecibirMPT" />
 
                                                                             <span href="#" class="dropdown-item" data-toggle="tooltip" title="Aprobar">
-                                                                                <button type="submit" class="btn btn-success btn-block " id="APROBARURLPT" name="APROBARURLPT" <?php echo $DISABLEDFOLIO; ?>>
+                                                                                <button type="submit" class="btn btn-success btn-block " id="APROBARURL" name="APROBARURL" <?php echo $DISABLEDFOLIO; ?>>
                                                                                     <i class="fa fa-check"></i>
                                                                                 </button>
                                                                             </span>
                                                                             <span href="#" class="dropdown-item" data-toggle="tooltip" title="Rechazar">
-                                                                                <button type="submit" class="btn btn-danger btn-block " id="RECHAZARURLPT" name="RECHAZARURLPT" <?php echo $DISABLEDFOLIO; ?>>
+                                                                                <button type="submit" class="btn btn-danger btn-block " id="RECHAZARURL" name="RECHAZARURL" <?php echo $DISABLEDFOLIO; ?>>
                                                                                     <i class="fa fa-close"></i>
                                                                                 </button>
                                                                             </span>
