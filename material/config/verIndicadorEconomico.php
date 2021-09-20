b885cd017ed36e8958f72954494b12a33b492539
Gabriel Toledo muñoz <gabrielvalkunut94@gmail.com>
2021-09-20 14:58:02 -0300

    revisión de visualización de indicadores economicos

--- material/config/verIndicadorEconomico.php
+++ material/config/verIndicadorEconomico.php
@@ -3,17 +3,17 @@
         <div class="d-lg-flex mr-20 ml-10 d-none">
             <div class="chart-text mr-10">
                 <h6 class="mb-0"><small></small></h6>
-                <h4 class="mt-0 text-primary">
-                    <div id="DolarO"><?php echo $DOLAR; ?></div>
-                </h4>
+                <h10 class="mt-0 text-primary" style="font-size:12px;">
+                    <div id="Dolar"></div>
+                </h10>
             </div>
         </div>
         <div class="d-lg-flex mr-20 ml-10 d-none">
             <div class="chart-text mr-10">
                 <h6 class="mb-0"><small></small></h6>
-                <h4 class="mt-0 text-danger">
-                    <div id="Euro"><?php echo $EURO; ?></div>
-                </h4>
+                <h10 class="mt-0 text-danger" style="font-size:12px;">
+                    <div id="Euro"></div>
+                </h10>
             </div>
         </div>
     </div>
