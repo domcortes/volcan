cb3d6cc9f7937e66a7347411fff4b9a3cbcb131d
Daniel Cortes <domcc.88@icloud.com>
2021-09-29 18:19:10 -0300

    eliminacion de js

--- mantencion/view/js/captcha.js
+++
@@ -1,8 +0,0 @@
-grecaptcha.ready(function() {
-	grecaptcha.execute('6Lcvs7QbAAAAAJs5Irwyqbd6zRj-B6r7ju_O8yXL', {action: 'submit'}).then(function(token) {
-		if (token) {
-			document.getElementById('recaptcha').value = token;
-			//console.log("token", token);
-		}
-	});
-});
