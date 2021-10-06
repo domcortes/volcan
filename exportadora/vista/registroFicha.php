0c1ba930d23ea71411fcd979e61618e421e867aa
Gabriel Toledo muñoz <gabrielvalkunut94@gmail.com>
2021-10-06 15:33:56 -0300

    revisión de titulo de pagina de ficha en modulo exportadora

--- exportadora/vista/registroFicha.php
+++ exportadora/vista/registroFicha.php
@@ -467,7 +467,7 @@ if (isset($_POST)) {
 <html lang="es">
 
 <head>
-    <title>Registro Orden </title>
+    <title>Registro Ficha </title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="">
