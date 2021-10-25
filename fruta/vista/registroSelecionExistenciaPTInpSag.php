4ff137430f4d3f279a78163add2fc7f207a7a6c8
Daniel Cortes <domcc.88@icloud.com>
2021-10-24 22:54:32 -0300

    correcciones responsive

--- fruta/vista/registroSelecionExistenciaPTInpSag.php
+++ fruta/vista/registroSelecionExistenciaPTInpSag.php
@@ -95,29 +95,7 @@ $ARRAYTESTADOSAG = "";
 //OPERACIONES
 //OPERACION DE REGISTRO DE FILA
 
-if (isset($_REQUEST['AGREGAR'])) {
 
-    $IDINPSAG = $_REQUEST['IDP'];
-    if (isset($_REQUEST['SELECIONAREXISTENCIA'])) {
-        $SINO = "0";
-        $SELECIONAREXISTENCIA = $_REQUEST['SELECIONAREXISTENCIA'];
-    } else {
-        $SINO = "1";
-        $MENSAJE = "DEBE  SELECIONAR UN REGISTRO";
-    }
-    if ($SINO == "0") {
-        foreach ($SELECIONAREXISTENCIA as $r) :
-            $IDEXIEXPORTACION = $r;
-            $EXIEXPORTACION->__SET('ID_INPSAG', $IDINPSAG);
-            $EXIEXPORTACION->__SET('ID_EXIEXPORTACION', $IDEXIEXPORTACION);
-            //LLAMADA AL METODO DE REGISTRO DEL CONTROLADOR
-            $EXIEXPORTACION_ADO->actualizarSelecionarSagCambiarEstado($EXIEXPORTACION);
-        endforeach;
-        $_SESSION["parametro"] =  $_REQUEST['IDP'];
-        $_SESSION["parametro1"] =  $_REQUEST['OPP'];
-        echo "<script type='text/javascript'> location.href ='" . $_REQUEST['URLO'] . ".php?op';</script>";
-    }
-}
 
 
 if (isset($_SESSION['parametro']) && isset($_SESSION['parametro1']) && isset($_SESSION['urlO'])) {
@@ -147,14 +125,10 @@ include_once "../config/validarDatosUrlD.php";
         <!- FUNCIONES BASES -!>
             <script type="text/javascript">
                 function mueveReloj() {
-
-
                     momentoActual = new Date();
-
                     dia = momentoActual.getDate();
                     mes = momentoActual.getMonth() + 1;
                     ano = momentoActual.getFullYear();
-
                     hora = momentoActual.getHours();
                     minuto = momentoActual.getMinutes();
                     segundo = momentoActual.getSeconds();
@@ -397,12 +371,12 @@ include_once "../config/validarDatosUrlD.php";
                                     <!-- /.box-body -->
                                     <div class="box-footer">
                                         <div class="btn-group btn-rounded btn-block col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 col-xs-12" role="group" aria-label="Acciones generales">
-                                            <button type="button" class="btn btn-rounded btn-success  " data-toggle="tooltip" title="Volver" name="CANCELAR" value="CANCELAR" Onclick="irPagina('<?php echo $URLO; ?>.php?op');">
-                                                <i class="ti-back-left "></i>
+                                            <button type="button" class="btn btn-success  " data-toggle="tooltip" title="Volver" name="CANCELAR" value="CANCELAR" Onclick="irPagina('<?php echo $URLO; ?>.php?op');">
+                                                <i class="ti-back-left "></i> Volver
                                             </button>
 
-                                            <button type="submit" class="btn btn-rounded btn-primary" data-toggle="tooltip" title="Seleccionar" name="AGREGAR" value="AGREGAR" <?php echo $DISABLED; ?>>
-                                                <i class="ti-save-alt"></i>
+                                            <button type="submit" class="btn btn-primary" data-toggle="tooltip" title="Seleccionar" name="AGREGAR" value="AGREGAR" <?php echo $DISABLED; ?>>
+                                                <i class="ti-save-alt"></i> Agregar
                                             </button>
                                         </div>
                                     </div>
@@ -419,6 +393,41 @@ include_once "../config/validarDatosUrlD.php";
     </div>
     <!- LLAMADA URL DE ARCHIVOS DE DISEÑO Y JQUERY E OTROS -!>
         <?php include_once "../config/urlBase.php"; ?>
+        <?php
+            if (isset($_REQUEST['AGREGAR'])) {
+                $IDINPSAG = $_REQUEST['IDP'];
+                if (isset($_REQUEST['SELECIONAREXISTENCIA'])) {
+                    $SINO = "0";
+                    $SELECIONAREXISTENCIA = $_REQUEST['SELECIONAREXISTENCIA'];
+                } else {
+                    $SINO = "1";
+                    $MENSAJE = "DEBE  SELECIONAR UN REGISTRO";
+                }
+                if ($SINO == "0") {
+                    foreach ($SELECIONAREXISTENCIA as $r) :
+                        $IDEXIEXPORTACION = $r;
+                        $EXIEXPORTACION->__SET('ID_INPSAG', $IDINPSAG);
+                        $EXIEXPORTACION->__SET('ID_EXIEXPORTACION', $IDEXIEXPORTACION);
+                        //LLAMADA AL METODO DE REGISTRO DEL CONTROLADOR
+                        $EXIEXPORTACION_ADO->actualizarSelecionarSagCambiarEstado($EXIEXPORTACION);
+                    endforeach;
+                    $_SESSION["parametro"] =  $_REQUEST['IDP'];
+                    $_SESSION["parametro1"] =  $_REQUEST['OPP'];
+                    echo '<script>
+                        Swal.fire({
+                            icon:"success",
+                            title:"Items Agregados",
+                            text:"El/los items seleccionados han sido agregado(s) correctamente"
+                        }).then((result)=>{
+                            if(result.value){
+                                location.href="'.$_REQUEST['URLO'].'.php?op";
+                            }
+                        })
+                    </script>';
+                    // echo "<script type='text/javascript'> location.href ='" . $_REQUEST['URLO'] . ".php?op';</script>";
+                }
+            }
+        ?>
 </body>
 
 </html>
