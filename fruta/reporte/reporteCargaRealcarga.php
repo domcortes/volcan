85159534c4aba51cbb88c87f194e5d4e068d492d
Gabriel Toledo muñoz <gabrielvalkunut94@gmail.com>
2021-11-26 12:25:26 -0300

    revisión de reporte de carga real de instructivo de carga

--- fruta/reporte/reporteCargaRealcarga.php
+++ fruta/reporte/reporteCargaRealcarga.php
@@ -67,7 +67,7 @@ if($_REQUEST['parametro']){
     $IDOP = $_REQUEST['parametro'];
    }
    
-   $ARRAYCONSOLIDADODESPACHO =  $DESPACHOEX_ADO->consolidadoDespachoExistencia2($IDOP);
+   $ARRAYCONSOLIDADODESPACHO =  $DESPACHOEX_ADO->consolidadoDespachoExistencia($IDOP);
    if($ARRAYCONSOLIDADODESPACHO){
    
      $ARRAYCONSOLIDADODESPACHOTOTAL =  $DESPACHOEX_ADO->obtenerTotalconsolidadoDespachoExistencia2($IDOP);
@@ -84,11 +84,14 @@ $html = '
 <table border="0" cellspacing="0" cellpadding="0">
     <thead>
     <tr>   
-        <th class="color center ">Fecha Embalado </th>
-        <th class="color center ">Cantidad Envase</th>
-        <th class="color center ">Kilos Neto</th>
-        <th class="color center ">Código Estandar </th>
+        <th class="color center ">Codigo Estandar </th>
         <th class="color center ">Envase/Estandar </th>
+        <th class="color center ">Peso Neto </th>
+        <th class="color center ">Peso Bruto </th>
+        <th class="color center ">Cantidad Envases </th>
+        <th class="color center ">Kilos Neto </th>
+        <th class="color center ">Kilos Bruto </th>
+        <th class="color center ">Fecha Embalado </th>
         <th class="color center ">CSG Productor </th>
         <th class="color center ">Nombre Productor </th>
         <th class="color center ">Variedad </th>
@@ -97,8 +100,8 @@ $html = '
  <tbody>
 
 ';
-foreach ($ARRAYCONSOLIDADODESPACHO as $r) :
-    $ARRAYPRODUCTOR = $PRODUCTOR_ADO->verProductor($r['ID_PRODUCTOR']);
+foreach ($ARRAYCONSOLIDADODESPACHO as $s) :
+    $ARRAYPRODUCTOR = $PRODUCTOR_ADO->verProductor($s['ID_PRODUCTOR']);
     if ($ARRAYPRODUCTOR) {
         $CSGPRODUCTOR = $ARRAYPRODUCTOR[0]['CSG_PRODUCTOR'];
         $NOMBREPRODUCTOR = $ARRAYPRODUCTOR[0]['NOMBRE_PRODUCTOR'];
@@ -106,32 +109,38 @@ foreach ($ARRAYCONSOLIDADODESPACHO as $r) :
         $CSGPRODUCTOR = "Sin Datos";
         $NOMBREPRODUCTOR = "Sin Datos";
     }
-    $ARRAYEEXPORTACION = $EEXPORTACION_ADO->verEstandar($r['ID_ESTANDAR']);
+    $ARRAYEEXPORTACION = $EEXPORTACION_ADO->verEstandar($s['ID_ESTANDAR']);
     if ($ARRAYEEXPORTACION) {
-        $CODIGOSTANDAR = $ARRAYEEXPORTACION[0]['CODIGO_ESTANDAR'];
-        $NOMBRESTANDAR = $ARRAYEEXPORTACION[0]['NOMBRE_ESTANDAR'];
+        $CODIGOESTANDAR = $ARRAYEEXPORTACION[0]['CODIGO_ESTANDAR'];
+        $NOMBREESTANTAR = $ARRAYEEXPORTACION[0]['NOMBRE_ESTANDAR'];
+        $NETOESTANTAR = $ARRAYEEXPORTACION[0]['PESO_NETO_ESTANDAR'];
+        $BRUTOESTANTAR = $ARRAYEEXPORTACION[0]['PESO_BRUTO_ESTANDAR'];
     } else {
-        $CODIGOSTANDAR = "Sin Datos";
-        $NOMBRESTANDAR = "Sin Datos";
+        $CODIGOESTANDAR = "Sin Datos";
+        $NOMBREESTANTAR = "Sin Datos";
+        $NETOESTANTAR = "Sin Datos";
+        $BRUTOESTANTAR = "Sin Datos";
     }
-    $ARRAYVERVESPECIESID = $VESPECIES_ADO->verVespecies($r['ID_VESPECIES']);
+    $ARRAYVERVESPECIESID = $VESPECIES_ADO->verVespecies($s['ID_VESPECIES']);
     if ($ARRAYVERVESPECIESID) {
         $NOMBREVARIEDAD = $ARRAYVERVESPECIESID[0]['NOMBRE_VESPECIES'];
     } else {
         $NOMBREVARIEDAD = "Sin Datos";
     }
 
-
     $html = $html . '    
             <tr class="center">
-                <td class="center">' . $r['EMBALADO'] . '</td>
-                <td class="center">' . $r['ENVASE'] . '</td>
-                <td class="center">' . $r['NETO'] . '</td> 
-                <td class="center">' . $CODIGOSTANDAR . '</td>
-                <td class="center">' . $NOMBRESTANDAR . '</td>
-                <td class="center">' . $CSGPRODUCTOR . ' </td>
-                <td class="center">' . $NOMBREPRODUCTOR . '</td>
-                <td class="center">' . $NOMBREVARIEDAD . '</td>
+                <td class="center">'.$CODIGOESTANDAR.'</td>
+                <td class="center">'.$NOMBREESTANTAR.'</td>
+                <td class="center">'.$NETOESTANTAR.'</td>
+                <td class="center">'.$BRUTOESTANTAR.'</td>
+                <td class="center">'.$s['ENVASE'].'</td>
+                <td class="center">'.$s['NETO'].'</td>
+                <td class="center">'.$s['BRUTO'].'</td>
+                <td class="center">'.$s['EMBALADO'].'</td>
+                <td class="center">'.$CSGPRODUCTOR.'</td>
+                <td class="center">'.$NOMBREPRODUCTOR.'</td>
+                <td class="center">'.$NOMBREVARIEDAD.'</td>
             </tr>
             ';
 endforeach;
