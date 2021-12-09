322b4ce6dc6441e72ba4b8284af4a10ca1f8bc55
Gabriel Toledo muñoz <gabrielvalkunut94@gmail.com>
2021-12-09 20:16:09 -0300

    reporte packing list despacho exportación

--- fruta/vista/listarDespachoEX.php
+++ fruta/vista/listarDespachoEX.php
@@ -327,6 +327,11 @@ include_once "../config/datosUrLP.php";
                                                                                     <i class="fa fa-file-pdf-o"></i> Comercial
                                                                                 </button>
                                                                             </span>
+                                                                            <span href="#" class="dropdown-item" data-toggle="tooltip" title="Packing List">
+                                                                                <button type="button" class="btn  btn-success btn-block" id="defecto" name="tarjas" title="Packing List"  Onclick="abrirPestana('../reporte/reporteDespachoExPackingList.php?parametro=<?php echo $r['ID_DESPACHOEX']; ?>&&usuario=<?php echo $IDUSUARIOS; ?>'); ">
+                                                                                    <i class="fa fa-file-excel-o"></i> Packing List
+                                                                                </button>
+                                                                            </span>
                                                                             <span href="#" class="dropdown-item" data-toggle="tooltip" title="CSV">
                                                                                 <button type="button" class="btn  btn-success btn-block" id="defecto" name="tarjas" title="Archivo Plano" <?php echo $DISABLEDP; ?> <?php echo $DISABLEDT; ?> Onclick="abrirPestana('../../assest/csv/CsvDespachoEx.php?parametro=<?php echo $r['ID_DESPACHOEX']; ?>&&usuario=<?php echo $IDUSUARIOS; ?>'); ">
                                                                                     <i class="fa fa-file-excel-o"></i> Archivo Plano
